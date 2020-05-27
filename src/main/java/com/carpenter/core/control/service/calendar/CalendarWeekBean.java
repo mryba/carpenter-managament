43b901e4f0e1a2f22a969fb4886e1882c4ed82d4
mryba <ryba.marcin20@gmail.com>
2020-05-27 21:35:54 +0200

    Add table in calendar

--- src/main/java/com/carpenter/core/control/service/calendar/CalendarWeekBean.java
+++ src/main/java/com/carpenter/core/control/service/calendar/CalendarWeekBean.java
@@ -5,6 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 @Named("calendarWeekBean")
 @ViewScoped
@@ -18,4 +24,13 @@ public class CalendarWeekBean extends CalendarBean {
         initCalendarMode(Mode.WEEK);
     }
 
+    public List<Date> getDates() {
+        List<Date> dates = new LinkedList<>();
+        LocalDateTime startDate = timeManager.getViewStartDate();
+        while (startDate.isBefore(timeManager.getViewEndDate())) {
+            dates.add(Date.from(startDate.atZone(ZoneId.systemDefault()).toInstant()));
+            startDate = startDate.plusDays(1);
+        }
+        return dates;
+    }
 }
