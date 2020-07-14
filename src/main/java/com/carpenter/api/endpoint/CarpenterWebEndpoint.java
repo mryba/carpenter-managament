1826c0d0707eeb072d944d1019ad8c10e946c603
rybsonM <ryba.marcin20@gmail.com>
2020-07-14 11:17:08 +0200

    remove usless pom dep, add new endpoint

--- src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
+++ src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
@@ -5,9 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.RequestScoped;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -17,10 +15,37 @@ import javax.ws.rs.core.Response;
 public class CarpenterWebEndpoint {
 
     @POST
+    @Path("/post")
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @NotNull
     public Response sendMail(CarpenterOfferRequest request) {
+        return Response
+                .ok(request, MediaType.APPLICATION_JSON)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Headers",
+                        "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Methods",
+                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .build();
+    }
 
+    @OPTIONS
+    @Path("{path : .*}")
+    public Response options() {
+        return Response
+                .status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .build();
+    }
+
+    @GET
+    @Path("/hi")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hi() {
         return Response.status(Response.Status.OK).build();
     }
 }
