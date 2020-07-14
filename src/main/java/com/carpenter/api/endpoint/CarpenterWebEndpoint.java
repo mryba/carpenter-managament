677063f51cbef0215f8cd1b916d58e9ca502fb5c
mryba <ryba.marcin20@gmail.com>
2020-07-14 22:43:15 +0200

    Create mail dispatchBena

--- src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
+++ src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
@@ -2,28 +2,36 @@ package com.carpenter.api.endpoint;
 
 import com.carpenter.api.request.CarpenterOfferRequest;
 import com.carpenter.api.response.CarpenterOfferResponse;
+import com.carpenter.core.control.mail.MailDispatchBean;
 import com.google.gson.Gson;
-import com.sun.corba.se.impl.orbutil.ObjectWriter;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.RequestScoped;
-import javax.validation.constraints.NotNull;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.CacheResponse;
 
 @Path("/carpenter")
 @RequestScoped
 @Slf4j
 public class CarpenterWebEndpoint {
 
+    @Inject
+    MailDispatchBean mailDispatchBean;
+
     @POST
     @Path("/post")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response sendMail(CarpenterOfferRequest request) {
+
+        String sb = request.getArchType() +
+                request.getName() + "\n" +
+                request.getCompany() + "\n" +
+                request.getPhone() + "\n" +
+                request.getDescription();
+        mailDispatchBean.sandEmailToManager(sb);
 
         CarpenterOfferResponse response = new CarpenterOfferResponse(Boolean.TRUE, Boolean.TRUE);
         String json = new Gson().toJson(response);
---
+++ src/main/java/com/carpenter/core/control/mail/MailDispatchBean.java
@@ -0,0 +1,75 @@
+package com.carpenter.core.control.mail;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Resource;
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateless;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.Serializable;
+import java.util.Properties;
+
+@Slf4j
+@Stateless
+public class MailDispatchBean implements Serializable {
+
+    private static final long serialVersionUID = 4334946629620090091L;
+
+    private static final String SANDER_INFO_MAIL = "podkarpaccy.ciesle@gmail.com";
+    private static final String SENDING_EMAIL_ERROR_MESSAGE = "Error sending e-mail";
+
+    @Resource(mappedName = "java:jboss/mail/carpenterMS")
+    private transient Session mailSession;
+
+    private void init() {
+
+    }
+
+    @Asynchronous
+    public void sandEmail(String recipient, String subject, String content) {
+        log.info("Dispaching email with subject \"{}\" and contents: \"{}\" to: {}", subject, content, recipient);
+
+        final String username = "podkarpaccy.ciesle@gmail.com";
+        final String password = "szczescia20";
+
+        String host = "smtp.gmail.com";
+
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.disable", "false");
+
+        Session session = Session.getInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setSender(new InternetAddress(SANDER_INFO_MAIL));
+            message.setRecipients(Message.RecipientType.TO, recipient);
+            message.setSubject(subject);
+            message.setContent(content, "text/plain;charset=utf-8");
+
+            Transport.send(message);
+            log.info("Email sent");
+        } catch (MessagingException e) {
+            log.error(SENDING_EMAIL_ERROR_MESSAGE);
+        }
+    }
+
+    public void sandEmailToManager(String content) {
+        sandEmail(SANDER_INFO_MAIL, "Oferta", content);
+    }
+
+
+}
