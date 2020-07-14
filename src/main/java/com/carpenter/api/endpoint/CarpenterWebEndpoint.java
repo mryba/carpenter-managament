6c607b8de6bc8e1c29c727e0566d140899d6e7b7
rybsonM <ryba.marcin20@gmail.com>
2020-07-14 12:56:07 +0200

    Create carpenteroffer response

--- src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
+++ src/main/java/com/carpenter/api/endpoint/CarpenterWebEndpoint.java
@@ -1,6 +1,10 @@
 package com.carpenter.api.endpoint;
 
 import com.carpenter.api.request.CarpenterOfferRequest;
+import com.carpenter.api.response.CarpenterOfferResponse;
+import com.google.gson.Gson;
+import com.sun.corba.se.impl.orbutil.ObjectWriter;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.RequestScoped;
@@ -8,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.CacheResponse;
 
 @Path("/carpenter")
 @RequestScoped
@@ -19,8 +24,12 @@ public class CarpenterWebEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response sendMail(CarpenterOfferRequest request) {
+
+        CarpenterOfferResponse response = new CarpenterOfferResponse(Boolean.TRUE, Boolean.TRUE);
+        String json = new Gson().toJson(response);
+
         return Response
-                .ok(request, MediaType.APPLICATION_JSON)
+                .ok(json, MediaType.APPLICATION_JSON)
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Credentials", "true")
                 .header("Access-Control-Allow-Headers",
---
+++ src/main/java/com/carpenter/api/response/CarpenterOfferResponse.java
@@ -0,0 +1,14 @@
+package com.carpenter.api.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarpenterOfferResponse {
+
+    private Boolean validError;
+    private Boolean status;
+}
